da27d81083e2f177510635ea469b8a425d4af9a9
Aldis Sarja <aldis.sarja@gmail.com>
2022-10-01 19:54:58 +0300

    ProductController full API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Ramsey\Collection\Collection;
 
 class ProductController extends Controller
@@ -71,5 +72,96 @@ class ProductController extends Controller
                 'message' => 'Invalid product name'
             ]],
             404);
+    }
+
+    public function createAmount(Request $request)
+    {
+        $request->validate([
+                'name' => 'required',
+                'price' => 'required|gt:0',
+                'description' => 'required',
+                'VAT' => 'required',
+                'amount' => 'required|gt:0',
+            ]
+        );
+        for ($c = 0; $c < $request->get('amount'); $c++) {
+            $product = new Product([
+                'name' => $request->name,
+                'price' => $request->price,
+                'description' => $request->description,
+                'VAT' => $request->VAT,
+            ]);
+            $product->save();
+        }
+    }
+
+    public function remove(int $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+    }
+
+    public function reserve($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->reserved = Carbon::now();
+        $product->save();
+    }
+
+    public function reserveUnset($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->reserved = null;
+        $product->save();
+    }
+
+    public function changePrice(int $id, int $price)
+    {
+        $product = Product::findOrFail($id);
+        $product->price = $price;
+        $product->save();
+    }
+
+    public function changePriceByName(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|gt:0',
+        ]);
+
+        $products = Product::where([
+            ['reserved', '=', null],
+            ['name', '=', $request->name]
+        ])->get();
+
+        foreach ($products as $product) {
+            $product->price = $request->price;
+            $product->save();
+        }
+    }
+
+    public function changeVAT(int $id, int $VAT)
+    {
+        $product = Product::findOrFail($id);
+        $product->VAT = $VAT;
+        $product->save();
+    }
+
+    public function changeVATByName(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'VAT' => 'required',
+        ]);
+
+        $products = Product::where([
+            ['reserved', '=', null],
+            ['name', '=', $request->name]
+        ])->get();
+
+        foreach ($products as $product) {
+            $product->VAT = $request->VAT;
+            $product->save();
+        }
     }
 }
--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
-//use PHPUnit\Framework\TestCase;
 use Tests\Testcase;
 
 class ProductTest extends TestCase
@@ -46,6 +45,9 @@ class ProductTest extends TestCase
 
         $this->assertNotNull($product);
         $this->assertEquals('TV03', $product->name);
+        $this->assertEquals(38000, $product->price);
+        $this->assertEquals('Hello World!', $product->description);
+        $this->assertEquals(21, $product->VAT);
     }
 
     public function test_it_should_be_able_to_get_product_list()
@@ -58,16 +60,72 @@ class ProductTest extends TestCase
                 'VAT' => 21,
             ]));
 
-        $products = (new ProductController)->getAll();
+        $product = (new ProductController)->getAll()->first();
 
-        $this->assertNotNull($products);
+        $this->assertNotNull($product);
+
+        $this->assertEquals('TV05', $product->name);
+        $this->assertEquals(38000, $product->price);
+        $this->assertEquals('Hello World!', $product->description);
+        $this->assertEquals(21, $product->VAT);
+        $this->assertEquals(1, $product->amount);
+
     }
 
     public function test_it_should_be_able_to_get_one_type_of_product()
     {
+        (new ProductController)->create(
+            new Request([
+                'name' => 'Hammer',
+                'price' => 1200,
+                'description' => 'Hello World!',
+                'VAT' => 21,
+            ]));
+
         $product = (new ProductController)->getByName("Hammer");
         $this->assertNotNull($product);
 
+        $this->assertEquals('Hammer', $product->name);
+    }
+
+    public function test_it_should_be_able_to_get_one_type_of_two_products()
+    {
+        $request = new Request([
+            'name' => 'Hammer',
+            'price' => 1200,
+            'description' => 'Hello World!',
+            'VAT' => 21,
+        ]);
+        (new ProductController)->create($request);
+        (new ProductController)->create($request);
+
+        $product = (new ProductController)->getByName("Hammer");
+        $this->assertNotNull($product);
+
+        $this->assertEquals('Hammer', $product->name);
+        $this->assertEquals(2, $product->amount);
+    }
+
+    public function test_it_should_be_able_to_add_amount_of_products()
+    {
+        $request = new Request([
+            'name' => 'Hammer',
+            'price' => 1200,
+            'description' => 'Hello World!',
+            'VAT' => 21,
+            'amount' => 5,
+        ]);
+        (new ProductController)->createAmount($request);
+
+        $product = (new ProductController)->getByName("Hammer");
+        $this->assertNotNull($product);
+
+        $this->assertEquals('Hammer', $product->name);
+        $this->assertEquals(5, $product->amount);
+    }
+
+    public function test_it_should_be_able_to_remove_product()
+    {
         (new ProductController)->create(
             new Request([
                 'name' => 'Hammer',
@@ -84,5 +142,139 @@ class ProductTest extends TestCase
         ]);
 
         $this->assertEquals('Hammer', $product->name);
+
+        // Remove
+        (new ProductController)->remove($product->id);
+
+        $product = Product::firstWhere($product->id);
+
+        $this->assertNull($product);
+    }
+
+    public function test_it_should_be_able_to_reserve_product()
+    {
+        (new ProductController)->create(
+            new Request([
+                'name' => 'Hammer',
+                'price' => 1200,
+                'description' => 'Hello World!',
+                'VAT' => 21,
+            ]));
+
+        $product = (new ProductController)->getByName("Hammer");
+
+        // Reserve
+        (new ProductController)->reserve($product->id);
+
+        $product = Product::find($product->id);
+
+        $this->assertNotNull($product->reserved);
+    }
+
+    public function test_it_should_be_able_to_unset_reserved_flag_for_product()
+    {
+        (new ProductController)->create(
+            new Request([
+                'name' => 'Hammer',
+                'price' => 1200,
+                'description' => 'Hello World!',
+                'VAT' => 21,
+            ]));
+
+        $product = (new ProductController)->getByName("Hammer");
+        $id = $product->id;
+
+        (new ProductController)->reserve($id);
+        $product = Product::find($id);
+        $this->assertNotNull($product->reserved);
+
+        (new ProductController)->reserveUnset($id);
+        $product = Product::find($id);
+        $this->assertNull($product->reserved);
+    }
+
+    public function test_it_should_be_able_to_change_price_for_product()
+    {
+        (new ProductController)->create(
+            new Request([
+                'name' => 'Hammer',
+                'price' => 1200,
+                'description' => 'Hello World!',
+                'VAT' => 21,
+            ]));
+
+        $product = Product::firstWhere([
+            ['name', '=', 'Hammer']
+        ]);
+
+        (new ProductController)->changePrice($product->id, 1300);
+
+        $product = Product::find($product->id);
+
+        $this->assertEquals(1300, $product->price);
+    }
+
+    public function test_it_should_be_able_to_change_price_for_products_by_name()
+    {
+        $request = new Request([
+            'name' => 'Hammer',
+            'price' => 1200,
+            'description' => 'Hello World!',
+            'VAT' => 21,
+            'amount' => 3,
+        ]);
+        (new ProductController)->createAmount($request);
+
+        (new ProductController)->changePriceByName(new Request([
+            'name' => 'Hammer',
+            'price' => 1400,
+        ]));
+
+        $products = (new ProductController)->getAll();
+        foreach ($products as $product) {
+            $this->assertEquals(1400, $product->price);
+        }
+    }
+
+    public function test_it_should_be_able_to_change_VAT_for_product()
+    {
+        (new ProductController)->create(
+            new Request([
+                'name' => 'Hammer',
+                'price' => 1200,
+                'description' => 'Hello World!',
+                'VAT' => 21,
+            ]));
+
+        $product = Product::firstWhere([
+            ['name', '=', 'Hammer']
+        ]);
+
+        (new ProductController)->changeVAT($product->id, 13);
+
+        $product = Product::find($product->id);
+        $this->assertEquals(13, $product->VAT);
+    }
+
+    public function test_it_should_be_able_to_change_VAT_for_products_by_name()
+    {
+        $request = new Request([
+            'name' => 'Hammer',
+            'price' => 1200,
+            'description' => 'Hello World!',
+            'VAT' => 21,
+            'amount' => 3,
+        ]);
+        (new ProductController)->createAmount($request);
+
+        (new ProductController)->changeVATByName(new Request([
+            'name' => 'Hammer',
+            'VAT' => 13,
+        ]));
+
+        $products = (new ProductController)->getAll();
+        foreach ($products as $product) {
+            $this->assertEquals(13, $product->VAT);
+        }
     }
 }
